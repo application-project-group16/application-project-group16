f6a5c6d1a6aaebc7102b3f602749d7e75df8357a
warfora <t3seka00@students.oamk.fi>
2026-01-17 15:23:26 +0200

    add sport places screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/home/HomeScreen';
+import SportPlacesScreen from '../screens/home/SportPlacesScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <>
-      <HomeScreen />
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Gyms Nearby" component={SportPlacesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
